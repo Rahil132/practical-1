082c95fff842763d46ac5af85d659046c7132722
Rahil132 <baigrahil78@gmail.com>
2025-07-23 15:02:00 +0530

    Add files via upload

---
+++ rahil/intrest calculation by rahu/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float p,r,t,si;
+
+    printf("enter principal amount: \n");
+    scanf("%f" , &p);
+
+    printf("enter rate of intrest: \n");
+    scanf("%f" , &r);
+
+    printf("enter total intrest (in year): \n");
+    scanf("%f" , &t);
+
+    si = (p*r*t) / 100;
+
+    printf("simple intrest = %.2f \n" , si);
+
+
+    return 0;
+}
